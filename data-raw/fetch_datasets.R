6023fe878207d32beae7137d043b1ad7ed3cac84
RobWiederstein <khuon68@gmail.com>
2021-11-14 06:38:23 -0600

    comment out line

--- data-raw/fetch_datasets.R
+++ data-raw/fetch_datasets.R
@@ -1,7 +1,7 @@
 # code for wei
 #set key for FRED
-#fred_key <- Sys.getenv("FRED_API_KEY")
-fred_key <- ${{ secrets.FRED_API_KEY }}
+fred_key <- Sys.getenv("FRED_API_KEY")
+#fred_key <- {{ secrets.FRED_API_KEY }}
 # fetch wei
 fredr::fredr_set_key(fred_key)
 wei <- fredr::fredr(
