bf861e7203ad3ad51d5b70c9fc8ebed83e9d732c
RobWiederstein <khuon68@gmail.com>
2021-12-06 19:24:06 -0600

    add comments

--- R/app_server.R
+++ R/app_server.R
@@ -4,12 +4,20 @@
 #'     DO NOT REMOVE.
 #' @import shiny
 #' @noRd
-app_server <- function( input, output, session ) {
-  # Your application server logic
-	mod_wei_server("wei_ui_1")
-	mod_umcsent_server("umcsent_ui_1")
-	mod_csushpinsa_server("csushpinsa_ui_1")
-	mod_gdp_total_server("gdp_total_ui_1")
-	mod_gdp_per_capita_server("gdp_per_capita_ui_1")
-	mod_oecd_prices_cpi_server("oecd_prices_cpi_ui_1")
+app_server <- function(input, output, session) {
+  #overview
+  mod_overview_boxes_server("overview_boxes_ui_1")
+  # general
+  mod_gdp_total_server("gdp_total_ui_1")
+  mod_gdp_per_capita_server("gdp_per_capita_ui_1")
+  mod_oecd_kei_unempl_rate_server("oecd_kei_unempl_rate_ui_1")
+  mod_oecd_prices_cpi_server("oecd_prices_cpi_ui_1")
+  # housing
+  mod_csushpinsa_server("csushpinsa_ui_1")
+  # leading
+  mod_wei_server("wei_ui_1")
+  # sentiment
+  mod_umcsent_server("umcsent_ui_1")
+  # stocks
+  mod_wb_wdi_mkt_cap_server("wb_wdi_mkt_cap_ui_1")
 }
